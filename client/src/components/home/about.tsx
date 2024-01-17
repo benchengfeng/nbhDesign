35845ca9b458eb45c2c4ff71fbf8c0de675bda3e
benchengfeng <amine_bh@outlook.com>
2024-01-17 19:53:31 +0100

    update landing

--- client/src/components/home/about.tsx
+++ client/src/components/home/about.tsx
@@ -9,18 +9,18 @@ const items = [
     title: 'High Performance',
     content: 'Elevate your game with high-performance technology. Enjoy seamless, efficient and powerful experiences.',
   },
-  {
-    key: '2',
-    icon: <i className="fas fa-desktop"></i>,
-    title: 'Flat Design',
-    content: 'Simplify your aesthetic with flat design. Clean, modern, and minimal design elements for a fresh look.',
-  },
-  {
-    key: '3',
-    icon: <i className="fas fa-database"></i>,
-    title: 'Simplified Workflow',
-    content: 'Streamline your work with our intuitive and efficient workflow solution. Get more done, faster and with ease.',
-  },
+  // {
+  //   key: '2',
+  //   icon: <i className="fas fa-desktop"></i>,
+  //   title: 'Flat Design',
+  //   content: 'Simplify your aesthetic with flat design. Clean, modern, and minimal design elements for a fresh look.',
+  // },
+  // {
+  //   key: '3',
+  //   icon: <i className="fas fa-database"></i>,
+  //   title: 'Simplified Workflow',
+  //   content: 'Streamline your work with our intuitive and efficient workflow solution. Get more done, faster and with ease.',
+  // },
 ]
 
 function AppAbout() {
@@ -33,6 +33,7 @@ function AppAbout() {
         <div className="contentHolder">
           <p>Technology has revolutionized the way we live and work, providing us with new and innovative ways to solve problems, improve efficiency, and make our lives easier. From artificial intelligence to cloud computing, the rapid advancements in technology are changing the way we interact with the world around us. As we continue to push the boundaries of what is possible, we can expect new and exciting developments that will further shape the future of our world. Whether it's in healthcare, education, or any other industry, technology has the power to make a positive impact and create a better future for all of us. </p>
         </div>
+        <div className="aboutusBlock">
         <Row gutter={[16, 16]}>   
           {items.map(item => {
             return (
@@ -48,6 +49,7 @@ function AppAbout() {
             );
           })}
         </Row>
+          </div>
       </div>
     </div>
   );
--- client/src/components/home/hero.tsx
+++ client/src/components/home/hero.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'antd';
 
 import { Carousel } from 'antd';
 
@@ -9,11 +8,11 @@ const items = [
     title: 'Design Architecture',
     content: 'Enter an immersive interior design experience for clients seeking exceptional creativity. Our approach merges aesthetics and function, enriching experiences for you and your patrons. Embrace a design revolution powered by AI and VR, shaping purposeful spaces beyond traditional limits.',
   },
-  // {
-  //   key: '2',
-  //   title: 'Work better together. Schedule meetings',
-  //   content: "Collaborate and achieve your goals as a team. Join forces, share ideas and leverage each others strengths to reach new heights together. Let's work together and create a brighter future for us all.",
-  // },
+  {
+    key: '2',
+    title: 'Work better together. Schedule meetings',
+    content: "Collaborate and achieve your goals as a team. Join forces, share ideas and leverage each others strengths to reach new heights together. Let's work together and create a brighter future for us all.",
+  },
   // {
   //   key: '3',
   //   title: 'The best app to increase your productivity',
@@ -24,17 +23,13 @@ const items = [
 function AppHero() {
   return (
     <div id="hero" className="heroBlock">
-      <Carousel>
+      <Carousel autoplay={true} autoplaySpeed={5000}>
         {items.map(item => {
           return (
             <div key={item.key} className="container-fluid">
               <div className="content">
-                <h3>{item.title}</h3>
-                <p>{item.content}</p>
-                <div className="btnHolder">
-                  {/* <Button type="default" size="large" >Learn More</Button> */}
-                  <Button size="large"><i className="fas fa-desktop"></i> Watch a Demo</Button>
-                </div>
+                <h3 style={{ color: 'white', fontWeight: "bold"}}>{item.title}</h3>
+                <p style={{ color: 'white', fontStyle:"italic", fontWeight: "bold"}}>{item.content}</p> 
               </div>
             </div>  
           );
