6d9fd892313c8d2ba7b63c6e827716c156327c13
benchengfeng <amine_bh@outlook.com>
2024-01-17 18:14:02 +0100

    update encodeuri payload

--- client/src/api/sendMail.tsx
+++ client/src/api/sendMail.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import axios from "axios";
 
 const SendMailButton: React.FC = () => {
-//   const [name, setName] = useState<string>("");
-//   const [email, setEmail] = useState<string>("");
-//   const [message, setMessage] = useState<string>("");
-
-const name = "cheng_feng";
-const email = "aminebizerta@gmail.com";
-const message = "hello cheng feng"
+  const name = "cheng_feng";
+  const email = "aminebizerta@gmail.com";
+  const message = "hello cheng feng";
 
   const handleClick = async () => {
     try {
-      await axios.post("sendEmail.php", { name, email, message });
+      await axios.post(
+        "sendEmail.php",
+        `name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&message=${encodeURIComponent(message)}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
       alert("Email sent successfully!");
     } catch (error: any) {
       console.error(
