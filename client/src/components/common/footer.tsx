3a2324a50c903ddc798473d2b200a578ca6fecf0
benchengfeng <amine_bh@outlook.com>
2024-06-19 22:37:33 +0200

    add instagram link

--- client/src/components/common/footer.tsx
+++ client/src/components/common/footer.tsx
@@ -20,7 +20,7 @@ function AppFooter() {
           <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
           <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
           <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
-          <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
+          <li><a href="https:///www.instagram.com/nbhdesign13620/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a></li>
         </ul>
         <div className="copyright">Copyright &copy; 2023 CF</div>
         <BackTop>
